081ec6626c2ed7699f328a64a8de19a63db6dbb0
vyacheslav.rozhk@mail.ru <vyacheslav.rozhk@mail.ru>
2019-09-14 01:02:18 +0300

    Пилим дальше DAESH CREATIVE!

--- first.c
+++ first.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
-int main()
-{
+#include <stdlib.h>
+int step(int a, int b){
+  int rez = 0;
+  //to do
+  return rez;
+}
+int SecondInToTen(char *number){
+  int length = 0;
+  int rez = 0;
+  char per1[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+  for (int i = 0; *(number+i) != '\0'; i++){
+    length++;
+  }//находим максимальную разрядность числа
+  for(int i = 0; i < length; i++){
+    int helper = -1;//переводим в int
+    for(int j = 0; j < 10; j++){
+      if (*(number + i) == per1[j])
+        helper = j;
+    }
+    rez += step(2 * helper, length-1);
+  }
+  return rez;
+}
+
+int main(){
+
   printf("Hello there!");
   return 0;
 }
